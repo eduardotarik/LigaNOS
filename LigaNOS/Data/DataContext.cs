0d91a177289b0920d118bbc618cc6228527dca2d
eduardotarik <eduardotarik@outlook.pt>
2023-07-20 20:03:06 +0100

    First Controller and Database

---
+++ LigaNOS/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using LigaNOS.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LigaNOS.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Team> Teams { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ LigaNOS/Data/Entities/Team.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LigaNOS.Data.Entities
+{
+    public class Team
+    {
+        public int Id { get; set; }
+
+        public string Emblem { get; set; }
+
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Founded { get; set; }
+
+        public string Country { get; set; }
+
+        public string City { get; set; }
+
+        public string Stadium { get; set; }
+
+        
+    }
+}
