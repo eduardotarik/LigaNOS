194d60f021dd2468899b38413dd1673dcd9f9a4a
eduardotarik <eduardotarik@outlook.pt>
2023-08-24 19:54:15 +0100

    Search Buttons Placing And Some Colors

--- LigaNOS/wwwroot/js/site.js
+++ LigaNOS/wwwroot/js/site.js
@@ -1,4 +1,11 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    $('#games').DataTable();
+});
 
-// Write your JavaScript code.
+$(document).ready(function () {
+    $('#teams').DataTable();
+});
+
+$(document).ready(function () {
+    $('#players').DataTable();
+});
